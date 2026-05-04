03ce79495cdaac23c549a7528a367d9dbe451ba8
slshan0597 <103019078+slshan0597@users.noreply.github.com>
2026-05-04 16:00:33 +0900

    Update CharacterBase.cs

--- Scripts/Object/Character/CharacterBase.cs
+++ Scripts/Object/Character/CharacterBase.cs
@@ -524,9 +524,9 @@ public class CharacterBase : MonoBehaviour, ICharacterBase, IGravityable, IDamag
 
     // ******************************************************************************
     // 3-4-2) 메서드 -> 액션 -> 피격(Damage)
-    //    - 타입: Normal(Explode), PressDown(Foot)
-    //    - 루틴: 시작(TryDamage, Damage) -> 반복(_Damage) -> 종료(StopDamage)
-    //    - Normal 타입은 캐릭터 넉백, PressDown 타입은 캐릭터 압축
+    //    - 타입: Normal(Explode), Press Down(Foot)
+    //    - 루틴: 시작(Try Damage, Damage) -> 반복(_Damage) -> 종료(Stop Damage)
+    //    - Normal 타입은 캐릭터 넉백, Press Down 타입은 캐릭터 압축
     // ******************************************************************************
     public virtual bool TryDamage(Transform attacker, DamageType type)
     {
