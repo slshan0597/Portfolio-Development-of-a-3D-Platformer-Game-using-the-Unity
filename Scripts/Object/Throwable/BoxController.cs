60aa417298e055ff0208b8ddabe6081841a85842
slshan0597 <103019078+slshan0597@users.noreply.github.com>
2026-05-13 14:06:55 +0900

    Update BoxController.cs

--- Scripts/Object/Throwable/BoxController.cs
+++ Scripts/Object/Throwable/BoxController.cs
@@ -6,104 +6,35 @@ using UnityEngine;
 using Resources  = BoxController.Resources;
 using DamageType = IDamageable.Type;
 
-
+// //////////////////////////////////////////////////////////////////////////////
+// 1. 인터페이스(IThrowableBase 인터페이스 상속)
+// //////////////////////////////////////////////////////////////////////////////
 public interface IBoxController : IThrowableBase
 {
-    #region Property
-
+    // 프로퍼티
     // Component
     new Resources resources { get; }
-
-    #endregion
 }
 
-
+// //////////////////////////////////////////////////////////////////////////////
+// 2. 클래스(ThrowableBase 클래스 상속)
+// //////////////////////////////////////////////////////////////////////////////
 public class BoxController : ThrowableBase, IBoxController
 {
-    #region Definition
-
-    public new class Resources : ThrowableBase.Resources
-    {
-        #region Definition
-
-        public class Models : Dictionary<State, IModelController>
-        {
-            #region Field
-
-            public IBoxModelController destroy { get; }
-
-            #endregion
-
-
-            #region Constructor
-
-            public Models(Transform transform) : base()
-            {
-                destroy = transform.GetComponentInChildren<IBoxModelController>(true);
-
-                foreach (var model in transform.GetComponentsInChildren<IModelController>(true))
-                {
-                    string name = model.gameObject.name.Replace(" ", string.Empty);
-
-                    if (Enum.TryParse(name, out State type)) Add(type, model);
-                }
-            }
-
-            #endregion
-
-
-            #region Method
-
-            public void Set(State state)
-            {
-                foreach (var element in this)
-                {
-                    State type  = element.Key;
-                    var   model = element.Value;
-
-                    model.gameObject.SetActive(type == state);
-                }
-            }
-
-            #endregion
-        }
-
-        #endregion
-
-
-        #region Field
-
-        public Models            models { get; }
-        public IEffectController effect { get; }
-
-        #endregion
-
-
-        #region Constructor
-
-        public Resources(Transform transform) : base(transform)
-        {
-            models = new Models(transform.Find("Models"));
-            effect = transform.GetComponentInChildren<IEffectController>(true);
-        }
-
-        #endregion
-    }
-
-    #endregion
-
-
-    #region Field
-
+    // ==============================================================================
+    // 1) 필드
+    // ==============================================================================
+    // Component & Reference
     public new Resources resources { get; protected set; }
 
-    #endregion
-
-
-    #region Method
-
-    #region Initialization
-
+    // ==============================================================================
+    // 2) 메서드
+    //    - 부모 클래스의 함수들을 재정의하여 확장
+    // ==============================================================================
+    // ------------------------------------------------------------------------------
+    // 2-1) 메서드 -> 초기화
+    //    - 필드(컴포넌트 등) 초기화
+    // ------------------------------------------------------------------------------
     protected override void SetField()
     {
         base.SetField();
@@ -118,22 +49,21 @@ public class BoxController : ThrowableBase, IBoxController
         _mask = DamageType.Normal | DamageType.PressDown | DamageType.Explode;
     }
 
-    #endregion
-
-
-    #region Idle
-
+    // ------------------------------------------------------------------------------
+    // 2-2) 메서드 -> 액션
+    // ------------------------------------------------------------------------------
+    // ******************************************************************************
+    // 2-2-1) 메서드 -> 액션 -> 대기(Idle)
+    // ******************************************************************************
     public override void Idle()
     {
         base.Idle();
         resources.models.Set(state);
     }
 
-    #endregion
-
-
-    #region Destroy
-
+    // ******************************************************************************
+    // 2-2-2) 메서드 -> 액션 -> 파괴(Destroy)
+    // ******************************************************************************
     protected override IEnumerator _Destroy(bool useUnscaledTime)
     {
         resources.models.Set(state);
@@ -150,8 +80,4 @@ public class BoxController : ThrowableBase, IBoxController
 
         Destroy(gameObject);
     }
-
-    #endregion
-
-    #endregion
 }
