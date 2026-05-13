7bbc01a7c87fb38c6fae8cd08651966bfcc16268
slshan0597 <103019078+slshan0597@users.noreply.github.com>
2026-05-13 10:59:11 +0900

    Create Stage.HammerBroController.cs

---
+++ Scripts/Object/Character/Enemy/Stage.HammerBroController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using UnityEngine;
+
+
+namespace Stage
+{
+    public interface IHammerBroController : global::IHammerBroController
+    {
+        #region Property
+
+        // Reference
+        new IPlanetController planet { get; }
+
+        #endregion
+    }
+
+
+    public class HammerBroController : global::HammerBroController, ITriggerable
+    {
+        #region Field
+
+        public new IPlanetController planet { get; protected set; }
+
+        [Header("Trigger Setting")]
+        [SerializeField] protected bool _useTrigger;
+
+        public bool useTrigger { get { return _useTrigger; } }
+
+        #endregion
+
+
+        #region Method
+
+        #region Initialization
+
+        protected override void SetField()
+        {
+            base.SetField();
+
+            planet = GetComponentInParent<IPlanetController>(true);
+        }
+
+        #endregion
+
+
+        #region Action
+
+        #region Die
+
+        protected override IEnumerator _Die()
+        {
+            yield return base._Die();
+
+            if (useTrigger) planet.TryClear();
+        }
+
+        #endregion
+
+        #endregion
+
+        #endregion
+    }
+}
