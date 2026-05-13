30d075f29f43e3a8f177d5890f74bd8a7698e50d
slshan0597 <103019078+slshan0597@users.noreply.github.com>
2026-05-13 18:15:30 +0900

    Update ExitPipeController.cs

--- Scripts/Object/Transporter/ExitPipeController.cs
+++ Scripts/Object/Transporter/ExitPipeController.cs
@@ -1,3 +1,25 @@
+// //////////////////////////////////////////////////////////////////////////////
+// - 출구 기능만을 가진 오브젝트
+// - 해당 지점으로 이동 후 비활성화
+//
+// * 목차
+//    1. 인터페이스 ... Line 33
+//    2. 클래스 ....... Line 55
+//        1) 정의 ..... Line 60
+//        2) 필드 ..... Line 70
+//        3) 메서드 ... Line 87
+//            1- 이벤트 함수 ... Line 91
+//            2- 초기화 ........ Line 107
+//            3- 액션 .......... Line 133
+//                1_ 대기(Idle) ............ Line 136
+//                2_ 활성화(Appear) ........ Line 147
+//                3_ 비활성화(Disappear) ... Line 159
+//                4_ 전송(Transport) ....... Line 169
+//                    1-> 준비(Ready) ... Line 193
+//                    2-> 입장(Enter) ... Line 203
+//                    3-> 대기(Wait) .... Line 216
+//                    4-> 퇴장(Exit) .... Line 228
+// //////////////////////////////////////////////////////////////////////////////
 using System;
 using System.Collections;
 using UnityEngine;
@@ -6,47 +28,37 @@ using JumpSetting     = ExitPipeController.JumpSetting;
 using PlayerLandState = PlayerController.State.Land;
 using PlayerJumpState = PlayerController.State.Jump;
 
-
+// //////////////////////////////////////////////////////////////////////////////
+// 1. 인터페이스(IPipeController 인터페이스 상속)
+// //////////////////////////////////////////////////////////////////////////////
 public interface IExitPipeController : IPipeController
 {
-    #region Property
-
+    // 프로퍼티
     // Setting
     JumpSetting jumpSetting { get; }
-
-    #endregion
 }
 
-
+// //////////////////////////////////////////////////////////////////////////////
+// 2. 클래스(PipeController 클래스 상속)
+// //////////////////////////////////////////////////////////////////////////////
 public class ExitPipeController : PipeController, IExitPipeController
 {
-    #region Definition
-
+    // ==============================================================================
+    // 1) 정의
+    // ==============================================================================
     [Serializable] public struct JumpSetting
     {
-        #region Field
-
         [SerializeField]                 private float _force;
         [SerializeField, Range(0f, 90f)] private float _angle;
 
         public float force { get { return _force; } }
         public float angle { get { return _angle; } }
-
-        #endregion
-
-
-        #region Constructor
-
+        
         public JumpSetting(float force, float angle)
         {
             _force = force;
             _angle = angle;
         }
-
-        #endregion
-
-
-        #region Method
 
         public Vector3 GetForce(IPlayerController player)
         {
@@ -56,39 +68,37 @@ public class ExitPipeController : PipeController, IExitPipeController
 
             return player.transform.TransformDirection(force);
         }
-
-        #endregion
     }
 
-    #endregion
-
-
-    #region Field
-
+    // ==============================================================================
+    // 2) 필드
+    // ==============================================================================
+    // Setting
     [SerializeField] protected JumpSetting _jumpSetting;
 
     public JumpSetting jumpSetting { get { return _jumpSetting; } }
 
+    // etc.
     protected Coroutine jumpAction;
 
-    #endregion
-
-
-    #region Method
-
-    #region Event
-
+    // ==============================================================================
+    // 3) 메서드
+    //    - 부모 클래스의 함수들을 재정의하여 확장
+    // ==============================================================================
+    // ------------------------------------------------------------------------------
+    // 3-1) 메서드 -> 이벤트 함수
+    //    - 오브젝트 초기화
+    // ------------------------------------------------------------------------------
     protected override void Start()
     {
         base.Start();
         gameObject.SetActive(false);
     }
 
-    #endregion
-
-
-    #region Initialization
-
+    // ------------------------------------------------------------------------------
+    // 3-2) 메서드 -> 초기화
+    //    - 필드(컴포넌트 등) 초기화
+    // ------------------------------------------------------------------------------
     protected override void ResetField()
     {
         base.ResetField();
@@ -96,21 +106,21 @@ public class ExitPipeController : PipeController, IExitPipeController
         _jumpSetting = new JumpSetting(250f, 75f);
     }
 
-    #endregion
-
-
-    #region Disappear
-
+    // ------------------------------------------------------------------------------
+    // 3-3) 메서드 -> 액션
+    // ------------------------------------------------------------------------------
+    // ******************************************************************************
+    // 3-3-1) 메서드 -> 액션 -> 비활성화(Disappear)
+    // ******************************************************************************
     protected override IEnumerator _Disappear(float duration, bool useUnscaledTime)
     {
         yield return useUnscaledTime ? new WaitForSecondsRealtime(duration) : new WaitForSeconds(duration);
     }
 
-    #endregion
-
-
-    #region Exit
-
+    // ******************************************************************************
+    // 3-3-2) 메서드 -> 액션 -> 퇴장(Exit)
+    //    - 해당 지점으로 이동 후 오브젝트 비활성화(Disappear)
+    // ******************************************************************************
     public override Coroutine Exit(IPlayerController player)
     {
         gameObject.SetActive(true);
@@ -148,8 +158,4 @@ public class ExitPipeController : PipeController, IExitPipeController
 
         player.resources.Play(PlayerLandState.Light);
     }
-
-    #endregion
-
-    #endregion
 }
