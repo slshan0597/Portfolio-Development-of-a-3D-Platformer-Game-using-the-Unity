ec04a10de16574f7c5ac1ffb94a0b92228275e16
slshan0597 <103019078+slshan0597@users.noreply.github.com>
2026-05-26 17:10:04 +0900

    Update SceneDirector.cs

--- Scripts/Scene/Tutorial/SceneDirector.cs
+++ Scripts/Scene/Tutorial/SceneDirector.cs
@@ -1,3 +1,19 @@
+// //////////////////////////////////////////////////////////////////////////////
+// * 요약
+//    - 튜토리얼 씬 디렉터 클래스
+//    - 컨트롤 가이드(UI) 표시
+//
+// * 목차
+//    1. 인터페이스 ... Line 26
+//    2. 클래스 ....... Line 37
+//        1) 필드 ..... Line 42
+//        2) 메서드 ... Line 49
+//            1- 이벤트 함수 ....... Line 52
+//            2- 초기화 ............ Line 57
+//            3- 들어오기(Enter) ... Line 70
+//            4- 나가기(Exit) ...... Line 96
+//            5- 일시정지(Pause) ... Line 106
+// //////////////////////////////////////////////////////////////////////////////
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,19 +21,18 @@ using UnityEngine;
 using Game;
 using UnityEngine.SceneManagement;
 
-
 namespace Tutorial
 {
     using UI = SceneDirector.UI;
 
-
     public enum GuideType { None, Move, Jump1, Jump2, HipDrop, Attack }
 
-
+    // //////////////////////////////////////////////////////////////////////////////
+    // 1. 인터페이스(ISceneBase 인터페이스 상속)
+    // //////////////////////////////////////////////////////////////////////////////
     public interface ISceneDirector : ISceneBase 
     {
-        #region Property
-
+        // 프로퍼티
         // Component
         UI ui { get; }
 
@@ -25,43 +40,28 @@ namespace Tutorial
         IPlanetController planet { get; }
         IPlayerController player { get; }
 
-        #endregion
-
-
-        #region Method
-
+        // 메서드
         void OpenGuide(GuideType type);
-
-        #endregion
     }
 
-
+    // //////////////////////////////////////////////////////////////////////////////
+    // 2. 클래스(SceneBase 클래스 상속)
+    // //////////////////////////////////////////////////////////////////////////////
     public class SceneDirector : SceneBase, ISceneDirector
     {
-        #region Definition
-
+        // ==============================================================================
+        // 1) 내부 타입
+        // ==============================================================================
         public class UI : List<IUIBase>
         {
-            #region Field
-
             public IMainUIController  main  { get; }
             public IGuideUIController guide { get; }
-
-            #endregion
-
-
-            #region Constructor
 
             public UI(Transform transform) : base(transform.GetComponentsInChildren<IUIBase>(true))
             {
                 main  = transform.GetComponentInChildren<IMainUIController>(true);
                 guide = transform.GetComponentInChildren<IGuideUIController>(true);
             }
-
-            #endregion
-
-
-            #region Method
 
             public void Initialize() { foreach (var element in this) element.gameObject.SetActive(false); }
 
@@ -76,40 +76,27 @@ namespace Tutorial
                 main.root.content.gameObject.SetActive(!paused);
                 guide.root.content.gameObject.SetActive(!paused);
             }
-
-            #endregion
         }
 
-        #endregion
-
-
-        #region Field
-
+        // ==============================================================================
+        // 2) 필드
+        // ==============================================================================
+        // Component & Reference
         public UI                ui     { get; protected set; }
         public IPlanetController planet { get; protected set; }
         public IPlayerController player { get; protected set; }
 
-        #endregion
-
-
-        #region Method
-
-        #region Event
-
-        //protected override void Awake()
-        //{
-        //    if (GameDirector.instance == null) SceneManager.LoadScene("Game");
-
-        //    base.Awake();
-        //}
-
+        // ==============================================================================
+        // 3) 메서드
+        // ==============================================================================
+        // ------------------------------------------------------------------------------
+        // 3-1) 메서드 -> 이벤트 함수
+        // ------------------------------------------------------------------------------
         protected virtual void Update() { player.state.hitPoint = player.setting.maxHitPoint; }
 
-        #endregion
-
-
-        #region Initialization
-
+        // ------------------------------------------------------------------------------
+        // 3-2) 메서드 -> 초기화
+        // ------------------------------------------------------------------------------
         protected override void SetField()
         {
             base.SetField();
@@ -121,11 +108,10 @@ namespace Tutorial
             type = Type.Tutorial;
         }
 
-        #endregion
-
-
-        #region General
-
+        // ------------------------------------------------------------------------------
+        // 3-3) 메서드 -> 들어오기(Enter)
+        //    - 플레이어 초기화 및 배치
+        // ------------------------------------------------------------------------------
         public override Coroutine Enter(Type prev = Type.None)
         {
             ui.Initialize();
@@ -133,46 +119,8 @@ namespace Tutorial
             player.Set(planet.characterTarget);
             camera.Follow(player.cameraTarget);
 
-            //StartCoroutine(ForRecording());
-
             return base.Enter(prev);
         }
-
-        //private IEnumerator ForRecording()
-        //{
-        //    player.transform.position = new Vector3(151.5f, 0.4f, 0f);
-        //    player.transform.rotation = Quaternion.Euler(0f, -90f, 0f);
-        //    player.resources.transform.gameObject.SetActive(false);
-        //    player.state.overlap[PlayerController.State.Overlap.State.Immunize] = true;
-        //    Camera.main.transform.GetComponent<AudioListener>().enabled = false;
-
-        //    var testCamera = player.transform.Find("Camera");
-        //    var area = GameObject.Find("Planet").transform.Find("Objects").Find("Test Objects").GetChild(1).GetComponent<SphereCollider>();
-
-        //    var box = GameObject.Find("Planet").transform.Find("Objects").Find("Test Objects").GetChild(1);
-
-        //    box.transform.position += Vector3.up * 5f;
-
-        //    var interactable = GameObject.Find("Planet").transform.Find("Objects").Find("Test Objects").GetChild(1).GetComponent<IInteractable>();
-
-        //    yield return new WaitForSeconds(2);
-
-        //    player.Interact(interactable);
-
-        //    player.Attack(PlayerController.State.Attack.Spin);
-
-        //    var boss = area.transform.GetComponent<IBossBase>();
-
-        //    boss.Die();
-        //    player.Die(PlayerController.State.Die.Normal);
-
-        //    player.Damage(testCamera, IDamageable.Type.Normal);
-        //    area.radius = 200f;
-
-        //    yield return new WaitForSeconds(4);
-
-        //    area.radius = 0.1f;
-        //}
 
         protected override IEnumerator _Enter(Type prev)
         {
@@ -187,6 +135,9 @@ namespace Tutorial
             if (Application.platform == RuntimePlatform.Android) player.ui.virtualJoystick.Display(true);
         }
 
+        // ------------------------------------------------------------------------------
+        // 3-4) 메서드 -> 나가기(Exit)
+        // ------------------------------------------------------------------------------
         public override Coroutine Exit(Type next = Type.None)
         {
             next = (next == Type.None) ? Type.Lobby : next;
@@ -194,6 +145,9 @@ namespace Tutorial
             return base.Exit(next);
         }
 
+        // ------------------------------------------------------------------------------
+        // 3-5) 메서드 -> 일시정지(Pause)
+        // ------------------------------------------------------------------------------
         public override void Pause(bool paused, params IAudioBase[] exceptions)
         {
             base.Pause(paused, exceptions);
@@ -204,9 +158,10 @@ namespace Tutorial
             else        camera.Follow(player.cameraTarget);
         }
 
-        #endregion
-
-
+        // ------------------------------------------------------------------------------
+        // 3-6) 메서드 -> 가이드 열기(Open Guide)
+        //    - 현재 상황에 따른 컨트롤 가이드(UI) 표시
+        // ------------------------------------------------------------------------------
         public virtual void OpenGuide(GuideType type)
         {
             ui.guide.Display(type);
@@ -214,7 +169,5 @@ namespace Tutorial
             if (type == GuideType.Attack)
                 foreach (var enemy in planet.enemies) enemy.Walk();
         }
-
-        #endregion
     }
 }
