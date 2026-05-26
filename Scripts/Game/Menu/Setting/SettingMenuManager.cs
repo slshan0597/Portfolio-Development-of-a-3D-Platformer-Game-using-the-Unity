d64056d3390a4525851492e15adce01785f9ff29
slshan0597 <103019078+slshan0597@users.noreply.github.com>
2026-05-26 11:24:27 +0900

    Update SettingMenuManager.cs

--- Scripts/Game/Menu/Setting/SettingMenuManager.cs
+++ Scripts/Game/Menu/Setting/SettingMenuManager.cs
@@ -1,59 +1,62 @@
+// //////////////////////////////////////////////////////////////////////////////
+// * 요약
+//    - 게임의 설정 메뉴 클래스
+//
+// * 목차
+//    1. 인터페이스 ... Line 
+//    2. 클래스 ....... Line 
+//        1) 내부 타입 ... Line 
+//        2) 필드 ........ Line 
+//        3) 메서드 ...... Line 
+//            1- 초기화 ............... Line 
+//            2- 반복자(Enumerator) ... Line 
+// //////////////////////////////////////////////////////////////////////////////
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-
 namespace Game
 {
     using SettingType = SettingBase.Type;
 
-
+    // //////////////////////////////////////////////////////////////////////////////
+    // 1. 인터페이스(IMenuBase 인터페이스 상속)
+    // //////////////////////////////////////////////////////////////////////////////
     public interface ISettingMenuManager : IMenuBase, IEnumerable<ISettingBase>
     {
-        #region Property
-
+        // 프로퍼티
         // Component
         new ISettingMenuUIController ui      { get; }
         IGraphicSettingManager       graphic { get; }
         IAudioSettingManager         audio   { get; }
         IControlSettingManager       control { get; }
 
-        #endregion
-
-
-        #region Indexing
-
+        // 메서드
+        // Indexing
         ISettingBase this[SettingType type] => this.First(menu => menu.type == type);
-
-        #endregion
     }
 
-
+    // //////////////////////////////////////////////////////////////////////////////
+    // 2. 클래스(MenuBase 클래스 상속)
+    // //////////////////////////////////////////////////////////////////////////////
     public class SettingMenuManager : MenuBase, ISettingMenuManager
     {
-        #region Definition
-
+        // ==============================================================================
+        // 1) 내부 타입
+        //    - 반복자 정의
+        // ==============================================================================
         public class SystemMenusEnumerator : IEnumerator<ISettingBase>
         {
-            #region Field
-
+            // 필드
             public ISettingBase[] _settings;
 
             private int index = -1;
 
-            #endregion
-
-
-            #region Constructor
-
+            // 생성자
             public SystemMenusEnumerator(ISettingBase[] settings) { _settings = settings; }
 
-            #endregion
-
-
-            #region Method
-
+            // 메서드
             public bool MoveNext() { return ++index < _settings.Length; }
 
             public void Reset() { index = -1; }
@@ -72,29 +75,26 @@ namespace Game
             }
 
             public void Dispose() { }
-
-            #endregion
         }
 
-        #endregion
-
-
-        #region Field
-
+        // ==============================================================================
+        // 2) 필드
+        // ==============================================================================
+        // Component & Reference
         public new ISettingMenuUIController ui      { get; protected set; }
         public IGraphicSettingManager       graphic { get; protected set; }
         public new IAudioSettingManager     audio   { get; protected set; }
         public IControlSettingManager       control { get; protected set; }
 
+        // etc.
         protected ISettingBase[] _settings;
 
-        #endregion
-
-
-        #region Method
-
-        #region Initialization
-
+        // ==============================================================================
+        // 3) 메서드
+        // ==============================================================================
+        // ------------------------------------------------------------------------------
+        // 3-1) 메서드 -> 초기화
+        // ------------------------------------------------------------------------------
         protected override void SetField()
         {
             base.SetField();
@@ -106,19 +106,13 @@ namespace Game
             _settings = GetComponentsInChildren<ISettingBase>(true);
         }
 
-        #endregion
-
-
-        #region Enumerable
-
+        // ------------------------------------------------------------------------------
+        // 3-2) 메서드 -> 반복자(Enumerator)
+        // ------------------------------------------------------------------------------
         IEnumerator<ISettingBase> IEnumerable<ISettingBase>.GetEnumerator() { return GetEnumerator(); }
 
         IEnumerator IEnumerable.GetEnumerator() { return GetEnumerator(); }
 
         public SystemMenusEnumerator GetEnumerator() { return new SystemMenusEnumerator(_settings); }
-
-        #endregion
-
-        #endregion
     }
 }
