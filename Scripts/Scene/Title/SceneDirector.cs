06341632158089f69eea1dabbadaf1b144d81fed
slshan0597 <103019078+slshan0597@users.noreply.github.com>
2026-05-26 16:21:01 +0900

    Update SceneDirector.cs

--- Scripts/Scene/Title/SceneDirector.cs
+++ Scripts/Scene/Title/SceneDirector.cs
@@ -1,47 +1,62 @@
+// //////////////////////////////////////////////////////////////////////////////
+// * 요약
+//    - 타이틀 씬 디렉터 클래스
+//
+// * 목차
+//    1. 인터페이스 ... Line 
+//    2. 클래스 ....... Line 
+//        1) 내부 타입 ... Line 
+//        2) 필드 ........ Line 
+//        3) 메서드 ...... Line 
+//            1- 이벤트 함수 ....... Line 
+//            2- 초기화 ............ Line 
+//            3- 들어오기(Enter) ... Line 
+//            4- 나가기(Exit) ...... Line 
+//            5- 일시정지(Pause) ... Line 
+// //////////////////////////////////////////////////////////////////////////////
 using System.Collections;
 using UnityEngine;
 
 using Game;
 
-
 namespace Title
 {
     using CursorVisibleEventType = GameDirector.CursorVisibleEventType;
 
-
+    // //////////////////////////////////////////////////////////////////////////////
+    // 1. 인터페이스(ISceneBase 인터페이스 상속)
+    // //////////////////////////////////////////////////////////////////////////////
     public interface ISceneDirector : ISceneBase
     {
-        #region Property
-
+        // 프로퍼티
         // Component
         IMenuManager  menu { get; }
         IUIController ui   { get; }
-
-        #endregion
     }
 
-
+    // //////////////////////////////////////////////////////////////////////////////
+    // 2. 클래스(SceneBase 클래스 상속)
+    // //////////////////////////////////////////////////////////////////////////////
     public class SceneDirector : SceneBase, ISceneDirector
     {
-        #region Field
-
+        // ==============================================================================
+        // 2) 필드
+        // ==============================================================================
+        // Component & Reference
         public IMenuManager  menu { get; protected set; }
         public IUIController ui   { get; protected set; }
 
-        #endregion
-
-
-        #region Method
-
-        #region Event
-
+        // ==============================================================================
+        // 3) 메서드
+        // ==============================================================================
+        // ------------------------------------------------------------------------------
+        // 3-1) 메서드 -> 이벤트 함수
+        // ------------------------------------------------------------------------------
         protected virtual void Update() { camera.transform.Rotate(Vector3.up, 5f * Time.deltaTime); }
 
-        #endregion
-
-
-        #region Initialization
-
+        // ------------------------------------------------------------------------------
+        // 3-2) 메서드 -> 초기화
+        // ------------------------------------------------------------------------------
         protected override void SetField()
         {
             base.SetField();
@@ -52,11 +67,11 @@ namespace Title
             type = Type.Title;
         }
 
-        #endregion
-
-
-        #region Enter
-
+        // ------------------------------------------------------------------------------
+        // 3-3) 메서드 -> 들어오기(Enter)
+        //    - 타이틀(UI) 표시
+        //    - 메인 메뉴 오
+        // ------------------------------------------------------------------------------
         public override Coroutine Enter(Type prev)
         {
             menu.Initialize();
@@ -78,11 +93,9 @@ namespace Title
             menu.Open(true);
         }
 
-        #endregion
-
-
-        #region Exit
-
+        // ------------------------------------------------------------------------------
+        // 3-4) 메서드 -> 나가기(Exit)
+        // ------------------------------------------------------------------------------
         public override Coroutine Exit(Type next)
         {
             menu.Open(false);
@@ -90,19 +103,13 @@ namespace Title
             return base.Exit(next);
         }
 
-        #endregion
-
-
-        #region Pause
-
+        // ------------------------------------------------------------------------------
+        // 3-5) 메서드 -> 일시정지(Pause)
+        // ------------------------------------------------------------------------------
         public override void Pause(bool paused, params IAudioBase[] exceptions)
         {
             base.Pause(paused, exceptions);
             menu.ui.SetInteractables(!paused);
         }
-
-        #endregion
-
-        #endregion
     }
 }
