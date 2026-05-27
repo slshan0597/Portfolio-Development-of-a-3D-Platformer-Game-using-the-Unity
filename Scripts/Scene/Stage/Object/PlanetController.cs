568da7331c16de961c1cafb5d35156f29650ca87
slshan0597 <103019078+slshan0597@users.noreply.github.com>
2026-05-27 17:06:34 +0900

    Update PlanetController.cs

--- Scripts/Scene/Stage/Object/PlanetController.cs
+++ Scripts/Scene/Stage/Object/PlanetController.cs
@@ -1,9 +1,23 @@
+// //////////////////////////////////////////////////////////////////////////////
+// * 요약
+//    - 기존 클래스를 스테이지 씬 전용으로 확장(상속)
+//    - 필드 클리어 기능 추가 및 클리어 시 보상(Transporter or Goal) 활성화
+//
+// * 목차
+//    1. 인터페이스 ... Line 27
+//    2. 클래스 ....... Line 55
+//        1) 내부 타입 ... Line 60
+//        2) 필드 ........ Line 122
+//        3) 메서드 ...... Line 143
+//            1- 이벤트 함수 ..... Line 146
+//            2- 초기화 .......... Line 184
+//            3- 중력(Gravity) ... Line 226
+// //////////////////////////////////////////////////////////////////////////////
 using System.Collections;
 using System.Linq;
 using UnityEngine;
 
 using Game;
-
 
 namespace Stage
 {
@@ -12,11 +26,12 @@ namespace Stage
     using SoundType        = SystemAudioController.SoundType;
     using BGMType          = BackGroundMusicController.SoundType;
 
-
+    // //////////////////////////////////////////////////////////////////////////////
+    // 1. 인터페이스(global::IPlanetController 클래스 상속)
+    // //////////////////////////////////////////////////////////////////////////////
     public interface IPlanetController : global::IPlanetController
     {
-        #region Property
-
+        // 프로퍼티
         // Component
         new IPlanetCoreController core         { get; }
         ICameraTargetController   cameraTarget { get; }
@@ -28,52 +43,47 @@ namespace Stage
         // Setting
         Type type { get; }
 
-        #endregion
-
-
-        #region Method
-
+        // 메서드
         void      Initialize(int index);
         void      Save(int index);
         bool      CheckClearable();
         void      TryClear();
         Coroutine Clear(bool animated = true);
-
-        #endregion
     }
 
-
+    // //////////////////////////////////////////////////////////////////////////////
+    // 2. 클래스(global::PlanetController 클래스 상속)
+    //    - CheckPointable : 필드 클리어 시 체크포인트 기능 활성화
+    // //////////////////////////////////////////////////////////////////////////////
     public class PlanetController : global::PlanetController, IPlanetController, ICheckPointable
     {
-        #region Definition
-
         public enum Type { Normal, Underground }
 
-        #endregion
-
-
-        #region Field
-
+        // ==============================================================================
+        // 1) 필드
+        // ==============================================================================
+        // Component & Reference
         public new IPlanetCoreController core         { get; protected set; }
         public ICameraTargetController   cameraTarget { get; protected set; }
         public IRewardable               reward       { get; protected set; }
         public ILevelController          level        { get; protected set; }
 
+        // Setting
         [SerializeField] protected Type _type;
         [SerializeField] protected bool _useCheckPoint;
 
         public Type type          { get { return _type; } }
         public bool useCheckPoint { get { return _useCheckPoint; } }
 
+        // etc.
         protected int triggeredCount, maxTriggeredCount;
 
-        #endregion
-
-
-        #region Method
-
-        #region Initialization
-
+        // ==============================================================================
+        // 2) 메서드
+        // ==============================================================================
+        // ------------------------------------------------------------------------------
+        // 2-1) 메서드 -> 초기화
+        // ------------------------------------------------------------------------------
         protected override void SetField()
         {
             base.SetField();
@@ -97,11 +107,10 @@ namespace Stage
             reward.gameObject.SetActive(data.level.planetsCleared[index]);
         }
 
-        #endregion
-
-
-        #region Set
-
+        // ------------------------------------------------------------------------------
+        // 2-2) 메서드 -> 셋(Set)
+        //    - 플레이어가 필드 상에 존재 시 활성화
+        // ------------------------------------------------------------------------------
         public override void SetEnable(bool enabled, global::IPlayerController player)
         {
             IBackGroundMusicController bgm = level.stage.scene.audios.bgm;
@@ -118,11 +127,10 @@ namespace Stage
             }
         }
 
-        #endregion
-
-
-        #region Data
-
+        // ------------------------------------------------------------------------------
+        // 2-3) 메서드 -> 저장하기(Save)
+        //    - 체크포인트 활성화 시 현재 필드의 진행 정보 저장
+        // ------------------------------------------------------------------------------
         public virtual void Save(int index)
         {
             if (reward == null) return;
@@ -132,11 +140,11 @@ namespace Stage
             data.level.planetsCleared[index] = reward.gameObject.activeInHierarchy;
         }
 
-        #endregion
-
-
-        #region Clear
-
+        // ------------------------------------------------------------------------------
+        // 2-4) 메서드 -> 성공(Clear)
+        //    - 필드 상의 클리어 트리거 발동 시 호출됨
+        //    - 클리어에 대한 보상(Transporter or Goal) 활성화
+        // ------------------------------------------------------------------------------
         public virtual bool CheckClearable()
         {
             if (reward == null)                      return false;
@@ -194,9 +202,5 @@ namespace Stage
 
             if (useCheckPoint) scene.SaveLevel(this);
         }
-
-        #endregion
-
-        #endregion
     }
 }
