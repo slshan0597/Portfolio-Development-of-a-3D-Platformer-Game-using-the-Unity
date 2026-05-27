80ae1e43182ef5f7649ffb4957b81a3faa50e13b
slshan0597 <103019078+slshan0597@users.noreply.github.com>
2026-05-27 15:50:17 +0900

    Update LevelController.cs

--- Scripts/Scene/Stage/Level/LevelController.cs
+++ Scripts/Scene/Stage/Level/LevelController.cs
@@ -1,16 +1,30 @@
+// //////////////////////////////////////////////////////////////////////////////
+// * 요약
+//    - 레벨 정보 클래스
+//    - 필드(플레닛), 체크포인트 등 정보 저장
+//
+// * 목차
+//    1. 인터페이스 ... Line 
+//    2. 클래스 ....... Line 
+//        1) 내부 타입 ... Line 
+//        2) 필드 ........ Line 
+//        3) 메서드 ...... Line 
+//            1- 초기화 ........... Line 
+//            2- 체크포인트 저장 ... Line 
+// //////////////////////////////////////////////////////////////////////////////
 using System.Collections.Generic;
 using UnityEngine;
-
 
 namespace Stage
 {
     using CheckPoints = LevelController.CheckPoints;
 
-
+    // //////////////////////////////////////////////////////////////////////////////
+    // 1. 인터페이스
+    // //////////////////////////////////////////////////////////////////////////////
     public interface ILevelController
     {
-        #region Property
-
+        // 프로퍼티
         // Component
         GameObject               gameObject  { get; }
         Transform                transform   { get; }
@@ -21,62 +35,43 @@ namespace Stage
         // Reference
         IStageController stage { get; }
 
-        #endregion
-
-
-        #region Method
-
+        // 메서드
         void Initialize();
         void Save(ICheckPointable checkPoint);
-
-        #endregion
     }
 
-
+    // //////////////////////////////////////////////////////////////////////////////
+    // 2. 클래스
+    // //////////////////////////////////////////////////////////////////////////////
     public class LevelController : MonoBehaviour, ILevelController
     {
-        #region Definition
-
+        // ==============================================================================
+        // 1) 내부 타입
+        // ==============================================================================
         public class CheckPoints : List<ICheckPointable>
         {
-            #region Field
-
             public ICheckPointable current;
 
-            #endregion
-
-
-            #region Constructor
-
             public CheckPoints(Transform transform) : base(transform.GetComponentsInChildren<ICheckPointable>(true)) { }
-
-            #endregion
         }
 
-        #endregion
-
-
-        #region Field
-
+        // ==============================================================================
+        // 2) 필드
+        // ==============================================================================
+        // Component & Reference
         public IIntroLauncherController launcher    { get; protected set; }
         public IPlanetController[]      planets     { get; protected set; }
         public CheckPoints              checkPoints { get; protected set; }
         public IStageController         stage       { get; protected set; }
 
-        #endregion
-
-
-        #region Method
-
-        #region Event
-
+        // ==============================================================================
+        // 3) 메서드
+        // ==============================================================================
         protected virtual void Awake() { SetField(); }
 
-        #endregion
-
-
-        #region Initialization
-
+        // ------------------------------------------------------------------------------
+        // 3-1) 메서드 -> 초기화
+        // ------------------------------------------------------------------------------
         protected virtual void SetField()
         {
             launcher    = GetComponentInChildren<IIntroLauncherController>(true);
@@ -100,11 +95,10 @@ namespace Stage
             for (int i = 0; i < planets.Length; i++) planets[i].Initialize(i);
         }
 
-        #endregion
-
-
-        #region Data
-
+        // ------------------------------------------------------------------------------
+        // 3-2) 메서드 -> 체크포인트 저장
+        //    - 현재 도달한 체크포인트 식별 번호와 필드(플래닛)의 정보를 저장
+        // ------------------------------------------------------------------------------
         public virtual void Save(ICheckPointable checkPoint)
         {
             IDataManager data = stage.scene.data;
@@ -116,9 +110,5 @@ namespace Stage
 
             for (int i = 0; i < planets.Length; i++) planets[i].Save(i);
         }
-
-        #endregion
-
-        #endregion
     }
 }
