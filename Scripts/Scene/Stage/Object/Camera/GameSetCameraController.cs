558cba62e65b10cbf744502acf417d6c4fc8b638
slshan0597 <103019078+slshan0597@users.noreply.github.com>
2026-06-01 11:25:17 +0900

    Update GameSetCameraController.cs

--- Scripts/Scene/Stage/Object/Camera/GameSetCameraController.cs
+++ Scripts/Scene/Stage/Object/Camera/GameSetCameraController.cs
@@ -1,3 +1,18 @@
+// //////////////////////////////////////////////////////////////////////////////
+// * 요약
+//    - 스테이지 씬 전용 게임 셋 카메라 클래스
+//    - 스테이지 클리어 또는 실패에 대한 카메라 연출 기능
+//    - UI, 배경(필드), 캐릭터(플레이어) 3가지 레이어(마스크)에 따른 내부 다중 카메라 배치
+//
+// * 목차
+//    1. 인터페이스 ... Line 28
+//    2. 클래스 ....... Line 44
+//        1) 필드 ..... Line 51
+//        2) 메서드 ... Line 61
+//            1- 초기화 ......... Line 64
+//            2- 셋(Set) ........ Line 82
+//            3- 회전(Rotate) ... Line 96
+// //////////////////////////////////////////////////////////////////////////////
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,56 +20,49 @@ using UnityEngine;
 
 using Game;
 
-
 namespace Stage
 {
     using LayerType = GameSetCameraController.LayerType;
 
-
+    // //////////////////////////////////////////////////////////////////////////////
+    // 1. 인터페이스(global::ICameraController 인터페이스 상속)
+    // //////////////////////////////////////////////////////////////////////////////
     public interface IGameSetCameraController : global::ICameraController
     {
-        #region Property
-
+        // 프로퍼티
         // Component
         Dictionary<LayerType, Camera> inners { get; }
 
         // Reference
         new ISceneDirector scene { get; }
 
-        #endregion
-
-
-        #region Method
-
+        // 메서드
         Coroutine Rotate(Vector3 euler, float duration = -1f);
-
-        #endregion
     }
 
-
+    // //////////////////////////////////////////////////////////////////////////////
+    // 2. 클래스(global::CameraController 클래스 상속)
+    // //////////////////////////////////////////////////////////////////////////////
     public class GameSetCameraController : global::CameraController, IGameSetCameraController
     {
-        #region Definition
-
         public enum LayerType { None, Player, UI, Other }
 
-        #endregion
-
-
-        #region Field
-
+        // ==============================================================================
+        // 1) 필드
+        // ==============================================================================
+        // Component & Reference
         public Dictionary<LayerType, Camera> inners { get; protected set; }
         public new ISceneDirector            scene  { get; protected set; }
 
+        // etc.
         protected Coroutine rotateAction;
 
-        #endregion
-
-
-        #region Method
-
-        #region Initialization
-
+        // ==============================================================================
+        // 2) 메서드
+        // ==============================================================================
+        // ------------------------------------------------------------------------------
+        // 2-1) 메서드 -> 초기화
+        // ------------------------------------------------------------------------------
         protected override void SetField()
         {
             base.SetField();
@@ -70,11 +78,11 @@ namespace Stage
             }
         }
 
-        #endregion
-
-
-        #region Set
-
+        // ------------------------------------------------------------------------------
+        // 2-2) 메서드 -> 셋(Set)
+        //    - 카메라 배치 시 카메라 활성화 및 현재 카메라로 설정
+        //    - UI 켄버스의 렌더 모드 변경(Overlay -> World Space)
+        // ------------------------------------------------------------------------------
         public override Coroutine Set(ICameraTargetController target, float duration = -1)
         {
             gameObject.SetActive(true);
@@ -84,11 +92,9 @@ namespace Stage
             return base.Set(target, duration);
         }
 
-        #endregion
-
-
-        #region Rotate
-
+        // ------------------------------------------------------------------------------
+        // 2-3) 메서드 -> 회전(Rotate)
+        // ------------------------------------------------------------------------------
         public virtual Coroutine Rotate(Vector3 euler, float duration = -1f)
         {
             if (rotateAction != null)
@@ -124,9 +130,5 @@ namespace Stage
             transform.rotation = endRotation;
             rotateAction       = null;
         }
-
-        #endregion
-
-        #endregion
     }
 }
