a005150c3357ac058f4c348d9bb264981fe53987
slshan0597 <103019078+slshan0597@users.noreply.github.com>
2026-06-01 11:12:37 +0900

    Update CameraController.cs

--- Scripts/Scene/Stage/Object/Camera/CameraController.cs
+++ Scripts/Scene/Stage/Object/Camera/CameraController.cs
@@ -1,45 +1,59 @@
+// //////////////////////////////////////////////////////////////////////////////
+// * 요약
+//    - 스테이지 씬 전용 메인 카메라 클래스
+//    - 플레이어 추격 등의 기능은 부모 클래스와 플레이어 타겟 클래스에서 이미 구현됨
+//    - 특수 이벤트에 대한 카메라 이동 기능 추가
+//
+// * 목차
+//    1. 인터페이스 ... Line 24
+//    2. 클래스 ....... Line 38
+//        1) 필드 ..... Line 43
+//        2) 메서드 ... Line 52
+//            1- 초기화 ............ Line 55
+//            2- 셋(Set) ........... Line 65
+//            3- 이동(Translate) ... Line 77
+// //////////////////////////////////////////////////////////////////////////////
 using System.Collections;
 using UnityEngine;
 
 using Game;
 
-
 namespace Stage
 {
+    // //////////////////////////////////////////////////////////////////////////////
+    // 1. 인터페이스(global::ICameraController 인터페이스 상속)
+    // //////////////////////////////////////////////////////////////////////////////
     public interface ICameraController : global::ICameraController
     {
-        #region Property
-
+        // 프로퍼티
         // Reference
         new ISceneDirector scene { get; }
 
-        #endregion
-
-
-        #region Method
-
+        // 메서드
         Coroutine Translate(ICameraTargetController target, float duration = -1f);
         void      StopTranslate();
-
-        #endregion
     }
 
-
+    // //////////////////////////////////////////////////////////////////////////////
+    // 2. 클래스(global::CameraController 클래스 상속)
+    // //////////////////////////////////////////////////////////////////////////////
     public class CameraController : global::CameraController, ICameraController
     {
-        #region Field
-
+        // ==============================================================================
+        // 1) 필드
+        // ==============================================================================
+        // Component & Reference
         public new ISceneDirector scene { get; protected set; }
 
+        // etc.
         protected Coroutine translateAction;
 
-        #endregion
-
-
-        #region Method
-
-        #region Initialization
-
+        // ==============================================================================
+        // 2) 메서드
+        // ==============================================================================
+        // ------------------------------------------------------------------------------
+        // 2-1) 메서드 -> 초기화
+        // ------------------------------------------------------------------------------
         protected override void SetField()
         {
             base.SetField();
@@ -47,11 +61,10 @@ namespace Stage
             scene = FindObjectOfType<SceneDirector>(true);
         }
 
-        #endregion
-
-
-        #region Set
-
+        // ------------------------------------------------------------------------------
+        // 2-2) 메서드 -> 셋(Set)
+        //    - 카메라 배치 시 카메라 활성화 및 현재 카메라로 설정
+        // ------------------------------------------------------------------------------
         public override Coroutine Set(ICameraTargetController target, float duration = -1)
         {
             gameObject.SetActive(true);
@@ -60,11 +73,11 @@ namespace Stage
             return base.Set(target, duration);
         }
 
-        #endregion
-
-
-        #region Translate
-
+        // ------------------------------------------------------------------------------
+        // 2-3) 메서드 -> 이동(Translate)
+        //    - Set 기능과 유사
+        //    - Set은 고정 타겟(좌표)에 대한 이동인 반면, Translate은 유동 타겟(좌표)에 대한 이동 기능
+        // ------------------------------------------------------------------------------
         public virtual Coroutine Translate(ICameraTargetController target, float duration = -1f)
         {
             StopTranslate();
@@ -105,9 +118,5 @@ namespace Stage
                 translateAction = null;
             }
         }
-
-        #endregion
-
-        #endregion
     }
 }
