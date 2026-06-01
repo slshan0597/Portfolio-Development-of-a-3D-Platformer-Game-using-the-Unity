cf9c61e3faf37ddf7957e4c0dc13fd43b6acf64b
slshan0597 <103019078+slshan0597@users.noreply.github.com>
2026-06-01 10:18:09 +0900

    Update SystemAudioController.cs

--- Scripts/Scene/Stage/Audio/SystemAudioController.cs
+++ Scripts/Scene/Stage/Audio/SystemAudioController.cs
@@ -1,57 +1,63 @@
+// //////////////////////////////////////////////////////////////////////////////
+// * 요약
+//    - 스테이지 씬 전용 시스템 오디오 클래스
+//    - 스테이지의 이벤트에 따른 오디오 재생
+//
+// * 목차
+//    1. 인터페이스 ... Line 22
+//    2. 클래스 ....... Line 38
+//        1) 필드 ..... Line 45
+//        2) 메서드 ... Line 56
+//            1- 초기화 ....... Line 59
+//            2- 재생(Play) ... Line 77
+// //////////////////////////////////////////////////////////////////////////////
 using UnityEngine;
-
 
 namespace Stage
 {
     using SoundType = SystemAudioController.SoundType;
     using AudioType = AudioBase.Type;
 
-
+    // //////////////////////////////////////////////////////////////////////////////
+    // 1. 인터페이스(IAudioBase 인터페이스 상속)
+    // //////////////////////////////////////////////////////////////////////////////
     public interface ISystemAudioController : IAudioBase
     {
-        #region Property
-
+        // 프로퍼티
         // Reference
         ISceneDirector scene { get; }
 
         // Setting
         SimpleData<SoundType, AudioClip> sounds { get; }
 
-        #endregion
-
-
-        #region Method
-
+        // 메서드
         float Play(SoundType type);
-
-        #endregion
     }
 
-
+    // //////////////////////////////////////////////////////////////////////////////
+    // 2. 클래스(AudioBase 클래스 상속)
+    // //////////////////////////////////////////////////////////////////////////////
     public class SystemAudioController : AudioBase, ISystemAudioController
     {
-        #region Definition
-
         public enum SoundType { None, PlanetClear, AppearBoss }
 
-        #endregion
-
-
-        #region Field
-
+        // ==============================================================================
+        // 1) 필드
+        // ==============================================================================
+        // Component & Reference
         public ISceneDirector scene { get; protected set; }
 
+        // Setting
         [SerializeField] protected SimpleData<SoundType, AudioClip> _sounds;
 
         public SimpleData<SoundType, AudioClip> sounds { get { return _sounds; } }
 
-        #endregion
-
-
-        #region Method
-
-        #region Initialization
-
+        // ==============================================================================
+        // 2) 메서드
+        // ==============================================================================
+        // ------------------------------------------------------------------------------
+        // 2-1) 메서드 -> 초기화
+        // ------------------------------------------------------------------------------
         protected override void SetField()
         {
             base.SetField();
@@ -67,20 +73,15 @@ namespace Stage
             _audioType               = AudioType.System;
         }
 
-        #endregion
-
-
-        #region Play
-
+        // ------------------------------------------------------------------------------
+        // 2-2) 메서드 -> 재생(Play)
+        //    - 이벤트에 따른 오디오 재생
+        // ------------------------------------------------------------------------------
         public virtual float Play(SoundType type)
         {
             audioSource.clip = sounds.ContainsKey(type) ? sounds[type] : null;
 
             return Play();
         }
-
-        #endregion
-
-        #endregion
     }
 }
